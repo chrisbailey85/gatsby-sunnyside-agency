6af5d055fd48f4c3feb8737f9210711090735254
Chris Bailey <chris85bailey@aol.co.uk>
2022-07-27 11:29:51 +0100

    corrected Banner path in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Banner from "../components/banner"
+import Banner from "../components/Banner"
 import ServicesCards from "../components/ServicesCards"
 import Testimonials from "../components/Testimonials"
 import DecorativeImages from "../components/DecorativeImages"
